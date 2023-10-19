a18912640db1c526a1d6e407cf109ab3837f16fd
imjhanC <jhan9502@gmail.com>
2023-10-19 12:47:50 +0800

    e

--- java project/src/windows_main.java
+++ java project/src/windows_main.java
@@ -1,10 +1,6 @@
 
 public class windows_main{
     public static void main(String[] args){
-<<<<<<< HEAD
         System.out.println("E e l ee n");
-=======
-        System.out.println("j");
->>>>>>> 7e6d68e1270a7daa606293596355f97490a145da
     }
 }
