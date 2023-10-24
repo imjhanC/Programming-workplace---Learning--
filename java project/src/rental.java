1a9b94c40990b5d78b6d4c1456c67094ddbcc053
imjhanC <jhan9502@gmail.com>
2023-10-24 22:17:35 +0800

    rental

---
+++ java project/src/rental.java
@@ -0,0 +1,21 @@
+public class rental {
+    private int current_year;
+    private int current_month;
+    private int electric_used_kwH;
+    private int bil_total_elec;
+    private int rate;
+    private int common_area_meter;
+
+    private int meter_reading_small_room;
+    private int electric_used_small_room;
+    private int price_to_pay_small_room;
+
+    private int meter_reading_medium_room;
+    private int electric_used_medium_room;
+    private int price_to_pay_medium_room;
+
+    private int meter_reading_master_room;
+    private int electric_used_master_room;
+    private int price_to_pay_master_room;
+
+}   
