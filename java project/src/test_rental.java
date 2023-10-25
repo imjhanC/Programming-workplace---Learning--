cb2c94ea02ed4755e68c7f4939aeb6156f00fdf2
imjhanC <jhan9502@gmail.com>
2023-10-25 16:32:03 +0800

    deleted the rental class

--- java project/src/test_rental.java
+++
@@ -1,8 +0,0 @@
-public class test_rental {
-    public static void main(String[] args){
-        rental rt = new rental(735,208.96);
-
-        double result = rt.get_Elec_rate(735, 208.96);
-        System.out.println(result);
-    }
-}
