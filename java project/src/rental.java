2293ca0a33b446b061e9c8f8386c17e26670b5c7
imjhanC <jhan9502@gmail.com>
2023-10-25 12:44:02 +0800

    rental

--- java project/src/rental.java
+++ java project/src/rental.java
@@ -1,21 +1,37 @@
 public class rental {
+    // main attributes
     private int current_year;
     private int current_month;
     private int electric_used_kwH;
-    private int bil_total_elec;
-    private int rate;
-    private int common_area_meter;
+    private double bil_total_elec;
+    private double rate;
+    private double common_area_meter;
 
-    private int meter_reading_small_room;
-    private int electric_used_small_room;
-    private int price_to_pay_small_room;
 
-    private int meter_reading_medium_room;
-    private int electric_used_medium_room;
-    private int price_to_pay_medium_room;
+    // small room
+    private double meter_reading_small_room;
+    private double electric_used_small_room;
+    private double price_to_pay_small_room;
 
-    private int meter_reading_master_room;
-    private int electric_used_master_room;
-    private int price_to_pay_master_room;
+    // medium room
+    private double meter_reading_medium_room;
+    private double electric_used_medium_room;
+    private double price_to_pay_medium_room;
 
+    // master room
+    private double meter_reading_master_room;
+    private double electric_used_master_room;
+    private double price_to_pay_master_room;
+
+    // name of tenants
+    private String tenantName;
+    
+    //water bill
+    private double water_bill;
+
+    public rental(){
+
+    }
+
+    
 }   
