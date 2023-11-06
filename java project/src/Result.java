e6520b535d54203a15670af6b4bb27167559bd31
imjhanC <jhan9502@gmail.com>
2023-11-06 13:02:54 +0800

    class

--- java project/src/Result.java
+++ java project/src/Result.java
@@ -68,6 +68,132 @@ public class Result {
         this.currentElectricRateCal = currentElectricRateCal;
     }
 
+    public double getcurrentCommonAreaMeter(){
+        return currentCommonAreaMeter;
+    }
+
+    public void setcurrentCommonAreaMeter(double currentCommonAreaMeter){
+        this.currentCommonAreaMeter = currentCommonAreaMeter;
+    }
+
+    public double getprevioussmallRoomMeterReading(){
+        return previoussmallRoomMeterReading;
+    }
+
+    public void setprevioussmallRoomMeterReading(double previoussmallRoomMeterReading){
+        this.previoussmallRoomMeterReading = previoussmallRoomMeterReading;
+    }
+
+    public double getcurrentsmallRoomMeterReading(){
+        return currentsmallRoomMeterReading;
+    }
+
+    public void setcurrentsmallRoomMeterReading(double currentsmallRoomMeterReading){
+        this.currentsmallRoomMeterReading = currentsmallRoomMeterReading;
+    }
+
+    public double getprevioussmallRoomElectricUsed(){
+        return previoussmallRoomElectricUsed;
+    }
+
+    public void setprevioussmallRoomElectricUsed(double previoussmallRoomElectricUsed){
+        this.previoussmallRoomElectricUsed = previoussmallRoomElectricUsed;
+    }
+
+    public double getcurrentsmallRoomElectricUsed(){
+        return currentsmallRoomElectricUsed;
+    }
+
+    public void setcurrentsmallRoomElectricUsed(double currentsmallRoomElectricUsed){
+        this.currentsmallRoomElectricUsed = currentsmallRoomElectricUsed;
+    }
+
+    public double getcurrentpriceToPaySmallRoomUsed(){
+        return currentpriceToPaySmallRoomUsed;
+    }
+
+    public void setcurrentpriceToPaySmallRoomUsed(double currentpriceToPaySmallRoomUsed){
+        this.currentpriceToPaySmallRoomUsed = currentpriceToPaySmallRoomUsed;
+    }
 
 
-}
+    public double getpreviousmediumRoomMeterReading(){
+        return previousmediumRoomMeterReading;
+    }
+
+    public void setpreviousmediumRoomMeterReading(double previousmediumRoomMeterReading){
+        this.previousmediumRoomMeterReading = previousmediumRoomMeterReading;
+    }
+
+    public double getcurrentmediumRoomMeterReading(){
+        return currentmediumRoomMeterReading;
+    }
+
+    public void setcurrentmediumRoomMeterReading(double currentmediumRoomMeterReading){
+        this.currentmediumRoomMeterReading = currentmediumRoomMeterReading;
+    }
+
+    public double getpreviousmediumRoomElectricUsed(){
+        return previousmediumRoomElectricUsed;
+    }
+
+    public void setpreviousmediumRoomElectricUsed(double previousmediumRoomElectricUsed){
+        this.previousmediumRoomElectricUsed = previousmediumRoomElectricUsed;
+    }
+
+    public double getcurrentmediumRoomElectricUsed(){
+        return currentmediumRoomElectricUsed;
+    }
+
+    public void setcurrentmediumRoomElectricUsed(double currentmediumRoomElectricUsed){
+        this.currentmediumRoomElectricUsed = currentmediumRoomElectricUsed;
+    }
+
+    public double getcurrentpriceToPayMediumRoomUsed(){
+        return currentpriceToPayMediumRoomUsed;
+    }
+
+    public void setcurrentpriceToPayMediumRoomUsed(double currentpriceToPayMediumRoomUsed){
+        this.currentpriceToPayMediumRoomUsed = currentpriceToPayMediumRoomUsed;
+    }
+
+    public double getpreviousmasterRoomMeterReading(){
+        return previousmasterRoomMeterReading;
+    }
+
+    public void setpreviousmasterRoomMeterReading(double previousmasterRoomMeterReading){
+        this.previousmasterRoomMeterReading = previousmasterRoomMeterReading;
+    }
+
+    public double getcurrentmasterRoomMeterReading(){
+        return currentmasterRoomMeterReading;
+    }
+
+    public void setcurrentmasterRoomMeterReading(double currentmasterRoomMeterReading){
+        this.currentmasterRoomMeterReading = currentmasterRoomMeterReading;
+    }
+
+    public double getpreviousmasterRoomElectricUsed(){
+        return previousmasterRoomElectricUsed;
+    }
+
+    public void setpreviousmasterRoomElectricUsed(double previousmasterRoomElectricUsed){
+        this.previousmasterRoomElectricUsed = previousmasterRoomElectricUsed;
+    }
+
+    public double getcurrentmasterRoomElectricUsed(){
+        return currentmasterRoomElectricUsed;
+    }
+
+    public void setcurrentmasterRoomElectricUsed(double currentmasterRoomElectricUsed){
+        this.currentmasterRoomElectricUsed = currentmasterRoomElectricUsed;
+    }
+
+    public double getcurrentpriceToPayMasterRoomUsed(){
+        return currentpriceToPayMasterRoomUsed;
+    }
+
+    public void setcurrentpriceToPayMasterRoomUsed(double currentpriceToPayMasterRoomUsed){
+        this.currentpriceToPayMasterRoomUsed = currentpriceToPayMasterRoomUsed;
+    }
+}   
