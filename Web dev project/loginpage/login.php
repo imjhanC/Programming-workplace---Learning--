8f1b0526db469cad807d8cf820d7832bdee4c9e3
imjhanC <chengjiahan@outlook.com>
2023-11-14 12:28:22 +0800

    changes

--- Web dev project/loginpage/login.php
+++ Web dev project/loginpage/login.php
@@ -15,7 +15,7 @@ $uname =validate($_POST['uname']);
 $pass =validate($_POST['password']);
 
 if(empty($uname)){
-    header ("Location: index.php?erro=User Name is required");
+    header ("Location: index.php?error=User Name is required");
     exit();
 }
 
