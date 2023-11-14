25f167af34ddcf3464230240d02e23fbc2ccc520
imjhanC <chengjiahan@outlook.com>
2023-11-14 12:27:12 +0800

    added home.php

--- Web dev project/loginpage/login.php
+++ Web dev project/loginpage/login.php
@@ -26,3 +26,24 @@ else if(empty($pass)){
 
 $sql ="SELECT * FROM users WHERE user_name='$uname' AND password='$pass' ";
 $result= mysqli_query($conn,$sql);
+
+if(mysqli_num_rows($result)===1){
+    $row =mysqli_fetch_assoc($result);
+    if($row['user_name']=== $uname && $row['password'] === $pass){
+        echo "Logged In !";
+        $_SESSION['user_name'] =$row['user_name'];
+        $_SESSION['name']=$row['name']; 
+        $_SESSION['id']=$row['id'];
+        header("Location: home.php");
+        exit();
+    }
+
+    else{
+        header("Location: index.php");
+        exit();
+    }
+}
+else{
+    header("Location: index.php");
+    exit();
+}
